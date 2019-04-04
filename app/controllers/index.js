7a84ee690c3a3c439046e0b055559c14d239b41a
Alberto Rincon <aluisrincon@unisangil.edu.co>
2019-04-03 19:33:17 -0500

    fix video camera bugs on Andoroid 7+

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,5 @@
-let videoBtn = Ti.UI.createButton({
-	title: "GRABAR"
-});
-let label = $.label;
-label.text = Alloy.Globals.materialIcons.play_circle_filled;
+
+
 
 
 function showCamera(type, callback) {
@@ -22,16 +19,19 @@ function showCamera(type, callback) {
 			error: function (e) {
 				callback(e, null);
 			},
-			saveToPhotoGallery: true,
 			mediaTypes: [type],
-			videoQuality: Titanium.Media.QUALITY_640x480,
+			videoQuality: Ti.Media.QUALITY_640x480,
 			// overlay: $.VideoContainer, //Se agrega en versiones de Android Actuales
-			whichCamera: Titanium.Media.CAMERA_FRONT
+			whichCamera: Titanium.Media.CAMERA_REAR,
+			saveToPhotoGallery: false,
 		});
 	};
 
+
+
+
 	// check if we already have permissions to capture media
-	if (!Ti.Media.hasCameraPermissions()) {
+	if (!Ti.Media.hasCameraPermissions() && !Ti.Filesystem.hasStoragePermissions()) {
 
 		// request permissions to capture media
 		Ti.Media.requestCameraPermissions(function (e) {
@@ -45,17 +45,32 @@ function showCamera(type, callback) {
 				callback(new Error('could not obtain camera permissions!'), null);
 			}
 		});
-	} else {
+	}
+	else {
 		camera();
 	}
 }
 
+function getVideoPath(_filename) {
+	let currentFilePath = null;
+	if (parseInt(Ti.Platform.version) < 6) {
+		console.info('pick for android version minor to 6');
+		currentFilePath = Ti.Filesystem.getFile(Ti.Filesystem.externalStorageDirectory, _filename);
+	} else {
+		console.info('pick for android version greater than 5');
+		currentFilePath = Ti.Filesystem.getFile(Ti.Filesystem.applicationCacheDirectory, _filename);
+	}
+	return currentFilePath;
+}
 
 
-videoBtn.addEventListener('click', function () {
+$.record.addEventListener('click', function () {
+
+	$.VideoContainer.removeAllChildren();
 
 	// attempt to capture video with the camera
 	showCamera(Ti.Media.MEDIA_TYPE_VIDEO, function (error, result) {
+		console.log("result: ", JSON.stringify(result));
 		if (error) {
 			alert('could not capture video');
 			return;
@@ -64,24 +79,31 @@ videoBtn.addEventListener('click', function () {
 		// validate we taken a video
 		if (result.mediaType == Ti.Media.MEDIA_TYPE_VIDEO) {
 
-			// create a videoPlayer to display our video
-			let videoPlayer = Ti.Media.createVideoPlayer({
-				url: result.media.nativePath,
-				autoplay: true
-			});
 
-			// add the imageView to the indexdow
-			$.VideoContainer.add(videoPlayer);
+
+			console.log("file: ", result.media.file.nativePath);
+			let sufixNameArray = result.media.file.nativePath.split("/");
+			let sufixName = sufixNameArray[sufixNameArray.length - 1];
+			console.log("sufixName: ", sufixName);
+			let _path = getVideoPath("video_result_" + sufixName);
+			console.log("_path: ", _path);
+			result.media.file.move(_path.nativePath);
+
+			if (_path.exists) {
+				console.log("Titanium.Filesystem.getFile(_path.nativePath).nativePath: ", Titanium.Filesystem.getFile(_path.nativePath).nativePath);
+				// create a videoPlayer to display our video
+				let videoPlayer = Ti.Media.createVideoPlayer({
+					url: Titanium.Filesystem.getFile(_path.nativePath).nativePath,
+					autoplay: true
+				});
+				console.log("videoPlayer.url", videoPlayer.url);
+				// add the imageView to the indexdow
+				$.VideoContainer.add(videoPlayer);
+			}
 		}
 	});
 });
 
-$.index.add(videoBtn);
-
-
-function doClick(e) {
-	alert($.label.text);
-}
 
 $.index.extendSafeArea = true;
 $.index.open();
